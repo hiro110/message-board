337d8af309c2519b26d60c1b82206fd577d2c8cb
h1roaki <h.ito198701@gmail.com>
2016-07-13 13:27:56 +0000

    add new column

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708084438) do
+ActiveRecord::Schema.define(version: 20160713124731) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
